55baf62e34ebb2265c34520b400db9e709005844
Abdul Wahab <aws18320@gmail.com>
2024-10-29 23:34:51 +0500

    Fixed errors for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,12 @@
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import mkcert from 'vite-plugin-mkcert'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    nodePolyfills(),
-    mkcert()
+    react()
   ],
- 
 })
